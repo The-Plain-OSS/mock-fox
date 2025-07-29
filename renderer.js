bb086c4da96ecc3d068c3193777059b29cddc14d
QyongGin <dydwlsdkrak03@gmail.com>
2025-07-29 15:33:08 +0900

    init: 초기 프로젝트 구성 및 UI 화면 업로드

---
+++ renderer.js
@@ -0,0 +1,29 @@
+// 렌더러 프로세스 (프론트엔드)
+
+// 사용자가 입력한 API 명세 수집
+
+// 사용자 입력값 수집 및 이벤트 처리
+
+// Save / Build 버튼 클릭 시 처리
+
+// (추후) Electron IPC를 통해 Go 백엔드와 연동하는 역할도 담당
+
+// 1. html 입력값 .value로 불러오고 data에 키:값쌍으로 딕셔너리 저장.
+document.getElementById("saveBtn").addEventListener("click", () => {
+  const data = {
+    endpoint: document.getElementById("endpoint").value,
+    method: document.getElementById("method").value,
+    query: document.getElementById("query").value,
+    body: document.getElementById("requestBody").value
+  };
+
+  // 2. 콘솔에 출력
+  console.log("명세 저장됨:", data);
+
+  // 3. Electron에 전달
+  window.api.send("save-spec", data);
+});
+
+document.getElementById("buildBtn").addEventListener("click", () => {
+  alert("Go 빌드 실행 요청됨!");
+});
